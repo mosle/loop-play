86f7c4f597b7ff85f74604cac3222edcbc31f4b5
mosle <yskysd@gmail.com>
2026-03-31 22:48:39 +0900

    Add tauri-plugin-process dependency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -668,10 +668,10 @@ fn setup_global_shortcuts(app: &tauri::AppHandle) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(ConfigPath(Mutex::new(None)))
         .invoke_handler(tauri::generate_handler![
             get_config,
